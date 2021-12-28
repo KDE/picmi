e8c141095811b0c1ff721830142b8721f6d201fd
Laurent Montel <montel@kde.org>
2021-12-28 13:23:35 +0100

    Migration code will be removed in qt6 + AA_UseHighDpiPixmaps is enabled by default in qt6

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,9 @@ int main(int argc, char *argv[])
     migrate.migrate();
 
     // Fixes blurry icons with fractional scaling
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
     QApplication app(argc, argv);
     KLocalizedString::setApplicationDomain("picmi");
 
